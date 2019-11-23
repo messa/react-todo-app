aa1b986e5ebc3d316cf52ce1b28cba001dbccf52
Petr Messner <petr.messner@gmail.com>
2019-11-23 12:06:35 +0100

    WIP

--- src/start/app.tsx
+++ src/start/app.tsx
@@ -26,10 +26,20 @@ const initialState = [
   }
 ];
 
+const Test = (props: { label: String }) => {
+  return (
+    <div>
+      <p><strong>{props.label}</strong></p>
+    </div>
+  )
+}
+
 export const App = () => {
   return (
     <div className="container">
+      <Test label='ahoj' />
       <h1>Hello world!</h1>
+      <pre>{JSON.stringify(initialState, null, 2)}</pre>
     </div>
   );
 };
